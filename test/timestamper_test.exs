0af34efa7e3f84eb293b86db6e8c4e49b060de73
Tim Mecklem <tim@teamgaslight.com>
2016-12-03 19:22:39 -0500

    Add test for timestamped across page boundaries

--- test/timestamper_test.exs
+++ test/timestamper_test.exs
@@ -3,6 +3,16 @@ defmodule TimestamperTest do
 
   doctest Timestamper
 
+  test "correctly timestamps across pages" do
+    {:ok, cgm_page_0} = Base.decode16("103D6A93089023") |> elem(1) |> Cgm.decode
+    {:ok, cgm_page_1} = Base.decode16("5455565713585A135A5B5B135C5C5D5D5D5D5D5B135A13595957565555545352525251504F4D4B494744423E3A3734312F2C282522201F1F1F2021212121212325262855901C44820E292B2D2CE50E101C53820F2D2E2F96EE") |> elem(1) |> Cgm.decode
+    {:ok, %{in_transition: in_transition}} = Timestamper.timestamp_events(cgm_page_1)
+    assert 72 == length(in_transition)
+    {:ok, %{processed: processed, in_transition: in_transition}} = Timestamper.timestamp_events(cgm_page_0, in_transition)
+    assert 0 == length(in_transition)
+    assert Enum.all?(processed, fn({_, %{timestamp: ts}}) -> !is_nil(ts) end)
+  end
+
   test "correctly assigns relative timestamps" do
     {:ok, cgm_page} = Base.decode16("1028B6140813131313133F77")
     {:ok, decoded_events} = Cgm.decode(cgm_page)
