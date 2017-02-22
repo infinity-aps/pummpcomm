6cad8e212dc09dab86f1402ddf2a24b50fde9356
Tim Mecklem <timothy@mecklem.com>
2017-02-21 23:04:45 -0500

    Decode BGReceived event
    Decode amount and alarm type more fully

--- lib/decocare/date_decoder.ex
+++ lib/decocare/date_decoder.ex
@@ -1,12 +1,12 @@
 defmodule Decocare.DateDecoder do
   use Bitwise
 
-  #  +=====================================================================+
-  #  | BYTE    0  |        |        1  |        |   2   |       | 3        |
-  #  | MONTH HIGH |   HOUR | MONTH LOW | MINUTE | FLAGS |   DAY |     YEAR |
-  #  + -----------+--------+-----------+--------+-------+-------+----------+
-  #  |         xx | 0xxxxx |        xx | xxxxxx |   xxx | xxxxx | 0xxxxxxx |
-  #  +=====================================================================+
+  #  +========================================================================+
+  #  | BYTE    0  |        |        1  |        |   2   |       | 3           |
+  #  | MONTH HIGH |   HOUR | MONTH LOW | MINUTE | FLAGS |   DAY | 2000 + YEAR |
+  #  + -----------+--------+-----------+--------+-------+-------+-------------+
+  #  |         xx | 0xxxxx |        xx | xxxxxx |   xxx | xxxxx |    0xxxxxxx |
+  #  +========================================================================+
 
   def decode_timestamp(timestamp = <<month_high::2, _::1, hour::5, month_low::2, minute::6, _flags::3, day::5, _::1, year::7>>) when is_binary(timestamp) do
     <<month::4>> = <<month_high::2, month_low::2>>
@@ -20,21 +20,17 @@ defmodule Decocare.DateDecoder do
   end
 
 
-  #  +========================================================================================+
-  #  | BYTE    0  |        |        1  |        |   2   |       |       | 3        | 3        |
-  #  | MONTH HIGH | SECOND | MONTH LOW | MINUTE | FLAGS |  HOUR | FLAGS |      DAY |     YEAR |
-  #  + -----------+--------+-----------+--------+-------+-------+-------+----------+----------+
-  #  |         xx | xxxxxx |        xx | xxxxxx |   xxx | xxxxx |   xxx |    xxxxx | 0xxxxxxx |
-  #  +========================================================================================+
+  #  +===================================================================================================+
+  #  | BYTE    0  |        |        1  |        |   2   |       |       | 3        | 4     |             |
+  #  | MONTH HIGH | SECOND | MONTH LOW | MINUTE | FLAGS |  HOUR | FLAGS |      DAY | FLAGS | 2000 + YEAR |
+  #  + -----------+--------+-----------+--------+-------+-------+-------+----------+-------+-------------+
+  #  |         xx | xxxxxx |        xx | xxxxxx |   xxx | xxxxx |   xxx |    xxxxx |     x |     xxxxxxx |
+  #  +===================================================================================================+
 
-  def decode_history_timestamp(timestamp = <<month_high::2, second::6, month_low::2, minute::6, _f1::3, hour::5, _f2::3, day::5, _ignore::1, year::7>>) when is_binary(timestamp) do
+  def decode_history_timestamp(timestamp = <<month_high::2, second::6, month_low::2, minute::6, _::3, hour::5, _::3, day::5, _::1, year::7>>) when is_binary(timestamp) do
     <<month::4>> = <<month_high::2, month_low::2>>
     case NaiveDateTime.new(2000 + year, month, day, hour, minute, second) do
       {:ok, timestamp} -> timestamp
     end
-  end
-
-  def decode_history_timestamp(timestamp) do
-    decode_history_timestamp(<<timestamp::40>>)
   end
 end
--- lib/decocare/history.ex
+++ lib/decocare/history.ex
@@ -1,9 +1,12 @@
 defmodule Decocare.History do
+  use Bitwise
+
   alias Decocare.Crc16, as: Crc16
   alias Decocare.DateDecoder, as: DateDecoder
 
   @cal_bg_for_ph 0x0A
   @alarm_sensor  0x0B
+  @bg_received   0x3F
 
   def decode(page) do
     case Crc16.check_crc_16(page) do
@@ -15,13 +18,48 @@ defmodule Decocare.History do
   def decode_page(page_data), do: decode_page(page_data, [])
   def decode_page(<<>>, events), do: events
 
-  def decode_page(<<@cal_bg_for_ph, amount::8, timestamp::40, tail::binary>>, events) do
-    event = {:cal_bg_for_ph, %{amount: amount, timestamp: DateDecoder.decode_history_timestamp(timestamp)}}
+  def decode_page(<<@cal_bg_for_ph, amount::8, timestamp::binary-size(5), tail::binary>>, events) do
+    <<_::size(16), amount_high_bit::size(1), _::size(15), amount_medium_bit::size(1), _::size(7)>> = timestamp
+    event_info = %{
+      amount: (amount_high_bit <<< 9) + (amount_medium_bit <<< 8) + amount,
+      timestamp: DateDecoder.decode_history_timestamp(timestamp),
+      raw: <<@cal_bg_for_ph::8, amount::8>> <> timestamp
+    }
+    event = {:cal_bg_for_ph, event_info}
     decode_page(tail, [event | events])
   end
 
-  def decode_page(<<@alarm_sensor::8, alarm_type::8, alarm_param::8, timestamp::40, tail::binary>>, events) do
-    event = {:alarm_sensor, %{timestamp: DateDecoder.decode_history_timestamp(timestamp)}}
+  @alarm_types %{
+    0x65 => "High Glucose",
+    0x66 => "Low Glucose",
+    0x68 => "Meter BG Now",
+    0x69 => "Cal Reminder",
+    0x6A => "Calibration Error",
+    0x6B => "Sensor End",
+    0x70 => "Weak Signal",
+    0x71 => "Lost Sensor",
+    0x73 => "Low Glucose Predicted"
+  }
+  def decode_page(<<@alarm_sensor::8, alarm_type::8, alarm_param::8, timestamp::binary-size(5), tail::binary>>, events) do
+    event_info = %{
+      timestamp: DateDecoder.decode_history_timestamp(timestamp),
+      alarm_type: Map.get(@alarm_types, alarm_type, "Unknown"),
+      raw: <<@alarm_sensor::8, alarm_type::8, alarm_param::8>> <> timestamp
+    }
+    event = {:alarm_sensor, event_info}
+    decode_page(tail, [event | events])
+  end
+
+
+  def decode_page(<<@bg_received, amount::8, timestamp::binary-size(5), meter_link_id::binary-size(3), tail::binary>>, events) do
+    <<_::size(16), amount_low_bits::size(3), _::size(21)>> = timestamp
+    event_info = %{
+      amount: (amount <<< 3) + amount_low_bits,
+      meter_link_id: Base.encode16(meter_link_id),
+      timestamp: DateDecoder.decode_history_timestamp(timestamp),
+      raw: <<@bg_received::8, amount::8>> <> timestamp <> meter_link_id
+    }
+    event = {:bg_received, event_info}
     decode_page(tail, [event | events])
   end
 end
