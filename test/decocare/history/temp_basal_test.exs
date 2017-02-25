06183185855159ca9fa87bb2e5f6569ae9edb347
Tim Mecklem <timothy@mecklem.com>
2017-02-25 01:18:35 -0500

    Add another temp basal test

--- test/decocare/history/temp_basal_test.exs
+++ test/decocare/history/temp_basal_test.exs
@@ -1,10 +1,16 @@
 defmodule Decocare.History.TempBasalTest do
   use ExUnit.Case
 
-  test "Temp Basal with absolute rate type" do
+  test "Temp Basal with absolute rate type - 1" do
     {:ok, history_page} = Base.decode16("33360C9A144D1100")
     decoded_events = Decocare.History.decode_page(history_page, %{})
     assert {:temp_basal, %{rate_type: :absolute, rate: 1.35, timestamp: ~N[2017-02-13 20:26:12], raw: ^history_page}} = Enum.at(decoded_events, 0)
+  end
+
+  test "Temp Basal with absolute rate type - 2" do
+    {:ok, history_page} = Base.decode16("336003AC144D1100")
+    decoded_events = Decocare.History.decode_page(history_page, %{})
+    assert {:temp_basal, %{rate_type: :absolute, rate: 2.4, timestamp: ~N[2017-02-13 20:44:03], raw: ^history_page}} = Enum.at(decoded_events, 0)
   end
 
   test "Temp Basal with percent rate type" do
