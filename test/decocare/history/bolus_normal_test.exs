53551a25b51c7da498b3e8ecb91498b52cc56e71
Tim Mecklem <timothy@mecklem.com>
2017-02-25 01:15:56 -0500

    Add another normal bolus test

--- test/decocare/history/bolus_normal_test.exs
+++ test/decocare/history/bolus_normal_test.exs
@@ -1,7 +1,7 @@
 defmodule Decocare.History.BolusNormalTest do
   use ExUnit.Case
 
-  test "Bolus Normal" do
+  test "Bolus Normal - 1" do
     {:ok, history_page} = Base.decode16("011D1D000785340D11")
     decoded_events = Decocare.History.decode_page(history_page, %{large_format: false, strokes_per_unit: 10})
     expected_event_info = %{
@@ -10,6 +10,20 @@ defmodule Decocare.History.BolusNormalTest do
       duration: 0,
       type: :normal,
       timestamp: ~N[2017-02-13 20:05:07],
+      raw: history_page
+    }
+    assert {:bolus_normal, ^expected_event_info} = Enum.at(decoded_events, 0)
+  end
+
+  test "Bolus Normal - 2" do
+    {:ok, history_page} = Base.decode16("011919002FA2340D11")
+    decoded_events = Decocare.History.decode_page(history_page, %{large_format: false, strokes_per_unit: 10})
+    expected_event_info = %{
+      programmed: 2.5,
+      amount: 2.5,
+      duration: 0,
+      type: :normal,
+      timestamp: ~N[2017-02-13 20:34:47],
       raw: history_page
     }
     assert {:bolus_normal, ^expected_event_info} = Enum.at(decoded_events, 0)
