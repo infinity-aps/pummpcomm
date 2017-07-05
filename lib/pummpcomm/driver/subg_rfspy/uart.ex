3466d176b1e07f953b58548645c1619b203ce511
Tim Mecklem <timothy@mecklem.com>
2017-07-05 17:48:04 -0400

    Calculate genserver timeout instead of constant

--- lib/pummpcomm/driver/subg_rfspy/uart.ex
+++ lib/pummpcomm/driver/subg_rfspy/uart.ex
@@ -2,8 +2,6 @@ defmodule Pummpcomm.Driver.SubgRfspy.UART do
   use GenServer
   alias Pummpcomm.Driver.SerialFraming
 
-  @genserver_timeout 60000
-
   def start_link do
     device = System.get_env("SUBG_RFSPY_DEVICE") || Keyword.get(Application.get_env(:pummpcomm, Pummpcomm.Driver.SubgRfspy.UART), :device)
     {:ok, serial_pid} = Nerves.UART.start_link
@@ -11,15 +9,15 @@ defmodule Pummpcomm.Driver.SubgRfspy.UART do
     :ok = Nerves.UART.configure(serial_pid, framing: {SerialFraming, separator: <<0x00>>})
     :ok = Nerves.UART.flush(serial_pid)
 
-    GenServer.start_link(__MODULE__, serial_pid, name: __MODULE__)
+    result = GenServer.start_link(__MODULE__, serial_pid, name: __MODULE__)
   end
 
   def write(data, timeout_ms) do
-    GenServer.call(__MODULE__, {:write, data, timeout_ms}, @genserver_timeout)
+    GenServer.call(__MODULE__, {:write, data, timeout_ms}, genserver_timeout(timeout_ms))
   end
 
   def read(timeout_ms) do
-    GenServer.call(__MODULE__, {:read, timeout_ms}, @genserver_timeout)
+    GenServer.call(__MODULE__, {:read, timeout_ms}, genserver_timeout(timeout_ms))
   end
 
   def handle_call({:write, data, timeout_ms}, _from, serial_pid) do
@@ -27,13 +25,17 @@ defmodule Pummpcomm.Driver.SubgRfspy.UART do
   end
 
   def handle_call({:read, timeout_ms}, _from, serial_pid) do
-    {:reply, Nerves.UART.read(serial_pid, timeout_ms + 10000), serial_pid}
+    {:reply, Nerves.UART.read(serial_pid, timeout_ms + 1000), serial_pid}
   end
 
   defp write_fully(data, timeout_ms, serial_pid) do
     case Nerves.UART.write(serial_pid, data, timeout_ms) do
-      :ok -> Nerves.UART.flush(serial_pid, :receive) # TODO: why do I have to flush receive here?
+      :ok -> Nerves.UART.flush(serial_pid, :receive)
       err -> err
     end
+  end
+
+  defp genserver_timeout(timeout_ms) do
+    timeout_ms + 2000
   end
 end
