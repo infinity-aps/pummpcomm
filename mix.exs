80d29734dab3377eeaea9feb0843bb0549e15449
Tim Mecklem <timothy@mecklem.com>
2017-08-01 23:32:06 -0400

    Bump elixir versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pummpcomm.Mixfile do
   def project do
     [app: :pummpcomm,
      version: "1.2.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
