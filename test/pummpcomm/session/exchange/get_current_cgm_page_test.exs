ba65e4203301e2e077d2daf91f9a69d6fa50f221
Tim Mecklem <timothy@mecklem.com>
2017-08-15 22:36:14 -0400

    Test that the command/response pair for getting current cgm page works

---
+++ test/pummpcomm/session/exchange/get_current_cgm_page_test.exs
@@ -0,0 +1,14 @@
+defmodule Pummpcomm.Driver.GetCurrentCgmPageTest do
+  use UartCaseTemplate, async: false
+
+  alias Pummpcomm.Session.PumpExecutor
+  alias Pummpcomm.Session.Exchange.GetCurrentCgmPage
+
+  doctest GetCurrentCgmPage
+
+  test "get current cgm page results in a number", %{pump_serial: pump_serial} do
+    {:ok, context} = pump_serial |> GetCurrentCgmPage.make() |> PumpExecutor.execute()
+    %{page_number: page_number} = GetCurrentCgmPage.decode(context.response)
+    assert is_integer(page_number)
+  end
+end
