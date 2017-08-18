24b07b8c8833b6cb48fea5ea954e1cfd6876a269
Tim Mecklem <timothy@mecklem.com>
2017-08-17 21:32:52 -0400

    Add ReadInsulinSensitivities command/response

---
+++ lib/pummpcomm/session/exchange/read_insulin_sensitivities.ex
@@ -0,0 +1,39 @@
+defmodule Pummpcomm.Session.Exchange.ReadInsulinSensitivities do
+  use Bitwise
+  alias Pummpcomm.Session.Command
+  alias Pummpcomm.Session.Response
+
+  @opcode 0x8B
+  def make(pump_serial) do
+    %Command{opcode: @opcode, pump_serial: pump_serial}
+  end
+
+  @max_count 8
+  def decode(%Response{opcode: @opcode, data: <<units::8, rest::binary>>}) do
+    %{units: decode_units(units), sensitivities: decode_sensitivity(rest, [], @max_count, units)}
+  end
+
+  defp decode_sensitivity(_, decoded_sensitivities, 0, _), do: Enum.reverse(decoded_sensitivities)
+  defp decode_sensitivity(<<_::2, 0::6, _::binary>>, decoded_sensitivities, _, _) when length(decoded_sensitivities) > 0, do: Enum.reverse(decoded_sensitivities)
+  defp decode_sensitivity(<<0::1, sensitivity_high::1, start_time::6, sensitivity_low::8, rest::binary>>, decoded_sensitivities, count, units) do
+    sensitivity = (sensitivity_high <<< 8) + sensitivity_low
+    decoded = %{start: basal_time(start_time), sensitivity: convert_sensitivity_value(units, sensitivity)}
+    decode_sensitivity(rest, [decoded | decoded_sensitivities], count - 1, units)
+  end
+
+  @mgdl 1
+  @mmol 2
+  defp decode_units(@mgdl), do: "mg/dL"
+  defp decode_units(@mmol), do: "mmol/L"
+
+  defp convert_sensitivity_value(@mgdl, sensitivity), do: sensitivity
+  defp convert_sensitivity_value(@mmol, sensitivity), do: sensitivity / 10
+
+  defp basal_time(raw_time) do
+    :local
+    |> Timex.now()
+    |> Timex.beginning_of_day()
+    |> Timex.shift(minutes: 30 * raw_time)
+    |> DateTime.to_time()
+  end
+end
---
+++ test/pummpcomm/session/exchange/read_insulin_sensitivities_test.exs
@@ -0,0 +1,24 @@
+defmodule Pummpcomm.Driver.ReadInsulinSensitivitiesTest do
+  use UartCaseTemplate, async: false
+
+  alias Pummpcomm.Session.PumpExecutor
+  alias Pummpcomm.Session.Response
+  alias Pummpcomm.Session.Exchange.ReadInsulinSensitivities
+
+  doctest ReadInsulinSensitivities
+
+  test "ReadInsulinSensitivities results in units and sensitivities", %{pump_serial: pump_serial} do
+    {:ok, context} = pump_serial |> ReadInsulinSensitivities.make() |> PumpExecutor.execute()
+    assert %{units: _, sensitivities: _} = ReadInsulinSensitivities.decode(context.response)
+  end
+
+  test "ReadInsulinSensitivities.decode returns correct information" do
+    {:ok, response_data} = Base.decode16("01002610222A26000000000000000000000000000000")
+    expected_sensitivities = [
+      %{start: ~T[00:00:00], sensitivity: 38},
+      %{start: ~T[08:00:00], sensitivity: 34},
+      %{start: ~T[21:00:00], sensitivity: 38}
+    ]
+    assert %{units: "mg/dL", sensitivities: expected_sensitivities} == ReadInsulinSensitivities.decode(%Response{opcode: 0x8B, data: response_data})
+  end
+end
