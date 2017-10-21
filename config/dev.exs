c38a7f75dce9557d585ad17bccddc19f6d359848
Tim Mecklem <timothy@mecklem.com>
2017-10-21 18:47:47 -0400

    [WIP] support for working with the rfm69 driver

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :logger, level: :debug
-config :subg_rfspy, :serial_driver, SubgRfspy.UART
+# config :subg_rfspy, :serial_driver, SubgRfspy.UART
 config :pummpcomm, :pump, Pummpcomm.Session.Pump
 config :pummpcomm, :cgm, Pummpcomm.Session.Pump
+config :pummpcomm, :pump_driver, RFM69
