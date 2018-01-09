d0e4dd9f22c44509d6ae32f6de72c77f1f56559b
Tim Mecklem <timothy@mecklem.com>
2018-01-09 13:23:25 -0500

    Support protocol at higher level easily by accepting struct as 1st param

--- lib/pummpcomm/monitor/blood_glucose_monitor.ex
+++ lib/pummpcomm/monitor/blood_glucose_monitor.ex
@@ -9,28 +9,32 @@ defmodule Pummpcomm.Monitor.BloodGlucoseMonitor do
   require Logger
   alias Pummpcomm.Cgm
   alias Pummpcomm.Cgm.Timestamper
+  alias Pummpcomm.Monitor.BloodGlucoseMonitor
 
-  def get_sensor_values(minutes_back, timezone) do
+  @enforce_keys [:cgm]
+  defstruct [:cgm]
+
+  def get_sensor_values(%BloodGlucoseMonitor{cgm: cgm}, minutes_back, timezone) do
     oldest_allowed = oldest_entry_allowed(minutes_back, timezone)
     Logger.info fn -> "Searching until we find a cgm entry older than #{inspect(oldest_allowed)}" end
 
-    {:ok, %{page_number: page_number}} = cgm().get_current_cgm_page()
-    {:ok, fetch_and_filter_page(page_number, [], oldest_allowed, page_number - 5)}
+    {:ok, %{page_number: page_number}} = cgm.get_current_cgm_page()
+    {:ok, fetch_and_filter_page(cgm, page_number, [], oldest_allowed, page_number - 5)}
   end
 
-  defp fetch_and_filter_page(-1, sensor_values, _, _), do: Enum.reverse(sensor_values)
-  defp fetch_and_filter_page(page_number, sensor_values, oldest_allowed, lowest_page_allowed) when page_number < lowest_page_allowed do
+  defp fetch_and_filter_page(_, -1, sensor_values, _, _), do: Enum.reverse(sensor_values)
+  defp fetch_and_filter_page(_, page_number, sensor_values, oldest_allowed, lowest_page_allowed) when page_number < lowest_page_allowed do
     Logger.warn fn -> "Reached max page fetches before finding an entry older than #{inspect(oldest_allowed)}" end
     Enum.reverse(sensor_values)
   end
 
-  defp fetch_and_filter_page(page_number, sensor_values, oldest_allowed, lowest_page_allowed) do
-    {:ok, values} = cgm().read_cgm_page(page_number)
+  defp fetch_and_filter_page(cgm, page_number, sensor_values, oldest_allowed, lowest_page_allowed) do
+    {:ok, values} = cgm.read_cgm_page(page_number)
     case Cgm.needs_timestamp?(values) do
       true ->
         Logger.info fn -> "Writing cgm timestamp on page #{page_number}" end
-        :ok = cgm().write_cgm_timestamp()
-        fetch_and_filter_page(page_number, sensor_values, oldest_allowed, lowest_page_allowed)
+        :ok = cgm.write_cgm_timestamp()
+        fetch_and_filter_page(cgm, page_number, sensor_values, oldest_allowed, lowest_page_allowed)
       false ->
         newest_first_values = Enum.reverse(values)
         {oldest_reached, sensor_values} = newest_first_values
@@ -38,7 +42,7 @@ defmodule Pummpcomm.Monitor.BloodGlucoseMonitor do
                                           |> filter_by_date(sensor_values, oldest_allowed)
         case oldest_reached do
           true -> Enum.reverse(sensor_values)
-          false -> fetch_and_filter_page(page_number - 1, sensor_values, oldest_allowed, lowest_page_allowed)
+          false -> fetch_and_filter_page(cgm, page_number - 1, sensor_values, oldest_allowed, lowest_page_allowed)
         end
     end
   end
@@ -58,9 +62,5 @@ defmodule Pummpcomm.Monitor.BloodGlucoseMonitor do
 
   defp oldest_entry_allowed(minutes_back, timezone) do
     timezone |> Timex.now() |> Timex.shift(minutes: -minutes_back) |> DateTime.to_naive
-  end
-
-  defp cgm do
-    Application.get_env(:pummpcomm, :cgm)
   end
 end
